6f34fc1f128f56c118276b569116b3eade7209ec
Grishma Dramce <grishma.dramce@gmail.com>
2025-05-23 00:41:36 +0100

    add TODOs

--- main.c
+++ main.c
@@ -1,45 +1,54 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
+// TODO: modulo retangulos
 #define MAX_RETANGULOS 10
 
+// TODO: modulo retangulos
 typedef struct {
     int id, x, y, l, h;
 } Retangulo;
 
+// TODO: modulo retangulos
 typedef struct {
     int quantidade;
     Retangulo lista[MAX_RETANGULOS];
 } Retangulos;
 
-
+// TODO: modulo mundo
 void limpaMundo(char mundo[][81]) {
     for (int y = 1; y <= 25; y++)
         for (int x = 1; x <= 80; x++)
-            mundo[y][x] = ' '; // limpar o mundo
+            mundo[y][x] = ' ';
 }
 
-void desenhaRetangulo(char mundo[][81], Retangulo retangulo) {
+// TODO: modulo mundo
+void desenhaRetangulo(char mundo[][81], const Retangulo retangulo) {
     for (int y = retangulo.y; y <= retangulo.y + retangulo.h - 1; y++)
         for (int x = retangulo.x; x <= retangulo.x + retangulo.l - 1; x++) {
-            if (x == retangulo.x || x == retangulo.x + retangulo.l - 1 || y == retangulo.y || y == retangulo.y +
-                retangulo.h - 1) // é extremidade?
-                mundo[26 - y][x] = 'X';
-            else if (x == retangulo.x + 1 && y == retangulo.y + retangulo.h - 2) // imprimir id
-                mundo[26 - y][x] = (char) ('0' + retangulo.id);
-            else // pintar retangulo
-                mundo[26 - y][x] = '+';
+            if (x == retangulo.x // TODO: criar funcao modulo retangulo "noContorno"
+                || x == retangulo.x + retangulo.l - 1
+                || y == retangulo.y
+                || y == retangulo.y + retangulo.h - 1
+            ) // é extremidade?
+                mundo[26 - y][x] = 'X'; // TODO: criar funcao no modulo mundo para fazer set
+            else
+                mundo[26 - y][x] = '+'; // pintar retangulo
         }
+    mundo[26 - (retangulo.y + retangulo.h - 2)][retangulo.x + 1] = (char) ('0' + retangulo.id); // imprimir id
 }
 
+// TODO: modulo mundo
 void desenhaRetangulos(const Retangulos *retangulos, char mundo[][81]) {
     for (int i = 0; i < retangulos->quantidade; i++)
         desenhaRetangulo(mundo, retangulos->lista[i]);
     fflush(stdout);
 }
 
+// TODO: modulo mundo
 void imprimeMundo(const Retangulos *retangulos) {
-    char mundo[26][81]; // para ignorar a primeira linha e primeira coluna para retirar o 0
+    char mundo[26][81]; // linha/coluna extra para ignorar a primeira linha e primeira coluna (deixando de usar o 0)
     limpaMundo(mundo);
     desenhaRetangulos(retangulos, mundo);
     // imprime
@@ -51,19 +60,23 @@ void imprimeMundo(const Retangulos *retangulos) {
     }
 }
 
-void criaRetangulo(Retangulos *retangulos, Retangulo retangulo) {
+// TODO: modulo retangulos
+void criaRetangulo(Retangulos *retangulos, const int x, const int y, const int l, const int h) {
     // TODO: detetar dentro das margens
-    // TODO: dar erro se chegarmos ao limite
-    retangulo.id = retangulos->quantidade; // TODO: procurar o primeiro id que nao existe
-    retangulos->lista[retangulos->quantidade] = retangulo;
+    // TODO: detetar quantidade maxima atingida
+    const Retangulo novoRetangulo = {
+        .x = x, .y = y, .l = l, .h = h,
+        .id = retangulos->quantidade // TODO: procurar o primeiro id que nao existe
+    };
+    // TODO: dar erro se nao estiver dentro das margens
+    retangulos->lista[retangulos->quantidade] = novoRetangulo;
     retangulos->quantidade++;
 }
 
 void trataCriaRetangulo(Retangulos *retangulos) {
-    Retangulo retangulo = {0};
-    // TODO: aceitar o formato certo
-    scanf("%d %d %d %d", &retangulo.x, &retangulo.y, &retangulo.l, &retangulo.h);
-    criaRetangulo(retangulos, retangulo);
+    int x, y, l, h;
+    scanf("%d %d %d %d", &x, &y, &l, &h); // TODO: aceitar o formato certo
+    criaRetangulo(retangulos, x, y, l, h);
     imprimeMundo(retangulos);
 }
 
@@ -72,7 +85,9 @@ int main() {
 
     Retangulos retangulos = {.quantidade = 0};
     char comando[100];
-    while (1) {
+    while (true) {
+        // TODO: imprimir menu
+        printf(">>  ");
         scanf("%s", comando);
         if (strcmp(comando, "create") == 0)
             trataCriaRetangulo(&retangulos);
